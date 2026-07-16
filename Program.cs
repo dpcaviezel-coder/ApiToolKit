b2912911aedd095decc311ce8f1aee65ded1d67a
dpcaviezel-coder <dpcaviezel@gmail.com>
2026-07-15 18:09:33 -0700

    Feature 1: Simple GET Request Tool

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Write("Enter a URL to GET: ");
+        string url = Console.ReadLine();
+
+        using var client = new HttpClient();
+
+        try
+        {
+            var response = await client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine($"\nStatus: {response.StatusCode}");
+            Console.WriteLine("\nResponse:");
+            Console.WriteLine(content);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"\nError: {ex.Message}");
+        }
+    }
+}
